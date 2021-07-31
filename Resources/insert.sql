ae6662980bff32bea3bc32c5a5ec0b3789f2c408
huang-feiyu <365429526@qq.com>
2021-07-31 12:42:30 +0800

    Update `mybook.db`

---
+++ Resources/insert.sql
@@ -0,0 +1,17 @@
+INSERT into author VALUES
+(49, "略萨", 24, 1936, 3, 28, 9999, 1, 1, 0);
+
+INSERT INTO book VALUES
+(172, "酒吧长谈", "null", 688, 482, 1, 1, 0, 172, 0, 1, 172, 49, 126);
+
+INSERT INTO my_info VALUES
+(172, 2021, 7, 31, 0, "null", 80);
+
+INSERT INTO release VALUES
+(172, 2021, 3, 978, 77, 02, 015947, 5);
+
+INSERT INTO country VALUES
+(24, "秘鲁");
+
+INSERT INTO translator VALUES
+(126, "孙家孟");
